214e334ba05de703668f1f488dea19abb1b4a34b
Khidr Brinkley <khidrbrinkley32@gmail.com>
2026-05-09 14:36:53 -0400

    initial commit

---
+++ IncomeUltra/IncomeUltra/IncomeUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeUltraApp.swift
+//  IncomeUltra
+//
+//  Created by Khidr Brinkley on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct IncomeUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
