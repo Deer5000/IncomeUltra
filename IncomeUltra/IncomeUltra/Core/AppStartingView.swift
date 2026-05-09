5a8b9ef5d4cac53f023ce0d50b6d8ddff6fef63c
Khidr Brinkley <khidrbrinkley32@gmail.com>
2026-05-09 14:58:35 -0400

    project setup

---
+++ IncomeUltra/IncomeUltra/Core/AppStartingView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IncomeUltra
+//
+//  Created by Khidr Brinkley on 5/9/26.
+//
+
+import SwiftUI
+
+struct AppStartingView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  AppStartingView()
+}
