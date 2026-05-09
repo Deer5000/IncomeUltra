cd701048d0dfecb1d92c56f8a4d29508d17c9d85
Khidr Brinkley <khidrbrinkley32@gmail.com>
2026-05-09 16:28:04 -0400

    View+Color extension

---
+++ IncomeUltra/IncomeUltra/Extensions/View/View+Color.swift
@@ -0,0 +1,96 @@
+//
+//  View+Color.swift
+//  IncomeUltra
+//
+//  Created by Khidr Brinkley on 5/9/26.
+//
+
+import SwiftUI
+
+extension Color {
+  static var appTheme: AppColorTheme = main
+}
+
+extension Color {
+  static var main: AppColorTheme {
+    .init(
+      accent: .init(.accent),
+      alternateAccent: .init(.alternateAccent),
+      viewBackground: .init(.viewBackground),
+      cellBackground: .init(.cellBackground),
+      text: .init(.text),
+      secondaryText: .init(.secondaryText),
+      alternateText: .init(.alternateText),
+      accentContrastText: .init(.accentContrastText),
+      primaryAction: .init(.primaryAction),
+      neutralAction: .init(.neutralAction),
+      destructive: .init(.destructive),
+      success: .init(.success),
+      warning: .init(.warning),
+      info: .init(.info),
+      error: .init(.error),
+      inProgres: .init(.inProgress),
+      divider: .init(.divider),
+      miscellaneous: .init(.miscellaneous)
+    )
+  }
+}
+
+struct AppColorTheme {
+  let accent: Color
+  let alternateAccent: Color
+  let viewBackground: Color
+  let cellBackground: Color
+  let text: Color
+  let secondaryText: Color
+  let alternateText: Color
+  let accentContrastText: Color
+  let primaryAction: Color
+  let neutralAction: Color
+  let destructive: Color
+  let success: Color
+  let warning: Color
+  let info: Color
+  let error: Color
+  let inProgres: Color
+  let divider: Color
+  let miscellaneous: Color
+}
+
+#Preview("Light Mode") {
+  Preview()
+    .preferredColorScheme(.light)
+}
+
+#Preview("Dark Mode") {
+  Preview()
+    .preferredColorScheme(.dark)
+}
+
+fileprivate struct Preview: View {
+  var body: some View {
+    VStack {
+      VStack {
+        Text("Title")
+          .foregroundColor(.appTheme.text)
+        Text("Subtitle")
+          .foregroundColor(.appTheme.secondaryText)
+      }
+
+      Divider()
+        .foregroundColor(.appTheme.divider)
+        .padding(.horizontal)
+
+      Button { } label: {
+        Text("Get Started")
+          .padding()
+          .background(Color.appTheme.accent)
+          .cornerRadius(8)
+          .foregroundColor(Color.accentContrastText)
+      }
+      .padding()
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(Color.appTheme.viewBackground)
+  }
+}
